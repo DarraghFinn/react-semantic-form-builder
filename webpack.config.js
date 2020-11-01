6f8720dcd249eb800a9fb3e4bbb7511d62df6512
finnjamin <finndarragh@gmail.com>
2020-11-01 17:45:56 +0000

    Updated webpack, package and dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,22 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: [path.resolve(__dirname, 'src/index.js')],
+  entry: [path.resolve(__dirname, "src/index.js")],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
     alias: {
-      '../../theme.config$': path.join(
-        __dirname,
-        'semantic-ui/theme.config',
-      ),
+      "../../theme.config$": path.join(__dirname, "semantic-ui/theme.config"),
     },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
       {
         test: /\.less$/,
@@ -27,27 +24,27 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          'css-loader',
-          'less-loader',
+          "css-loader",
+          "less-loader",
         ],
       },
-      { 
+      {
         test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-        use: 'url-loader',
+        use: "url-loader",
       },
-      { 
+      {
         test: /\.(png|j?g|gif|ico)?$/,
-        use: 'url-loader',
-      }
+        use: "url-loader",
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       inject: true,
-      template: path.resolve(__dirname, 'index.html'),
+      template: path.resolve(__dirname, "index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles/[name].[contenthash].css',
+      filename: "styles/[name].[contenthash].css",
     }),
   ],
 };
